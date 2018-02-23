c4d7b348520ca0087bbcf44901aba8f2f330fe8e
cowboyuniverse <cowboyuniverse@gmail.com>
2018-02-22 17:04:36 -0800

    adding updateDao and updateDaoImpl

---
+++ src/main/java/techit/model/dao/jpa/UpdateDaoImpl.java
@@ -0,0 +1,44 @@
+package techit.model.dao.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import techit.model.Update;
+import techit.model.User;
+import techit.model.dao.TicketDao;
+import techit.model.dao.UpdateDao; 
+
+
+@Repository
+public class UpdateDaoImpl implements UpdateDao {
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	
+	@Override
+	public Update getUpdate(Integer Id) {
+
+		return entityManager.find( Update.class, Id );
+	}
+
+
+	@Override
+	public List<Update> getUpdate() {
+	       return entityManager.createQuery( "from Updates order by update_id", Update.class )
+	                .getResultList();
+	}
+    
+
+    @Override
+    @Transactional
+    public Update saveUpdate( Update update )
+    {
+        return entityManager.merge( update );
+    }
+
+}
