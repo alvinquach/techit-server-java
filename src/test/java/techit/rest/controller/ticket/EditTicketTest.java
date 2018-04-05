25790138f6bf0f4ec6c6855011332417a5d8cb21
agarcia94 <mattgarcia94@sbcglobal.net>
2018-04-05 15:31:43 -0700

    added test cases for editing ticket

---
+++ src/test/java/techit/rest/controller/ticket/EditTicketTest.java
@@ -0,0 +1,77 @@
+package techit.rest.controller.ticket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import techit.authentication.TokenAuthenticationService;
+import techit.model.Priority;
+import techit.model.Status;
+import techit.model.Ticket;
+import techit.util.StringUtils;
+
+
+@Test(groups = "EditTicketTest")
+@WebAppConfiguration
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class EditTicketTest extends AbstractTransactionalTestNGSpringContextTests{
+	
+	@Autowired
+	private WebApplicationContext wac;
+
+	@Autowired
+	private TokenAuthenticationService tokenAuthenticationService;
+
+	private MockMvc mockMvc;
+
+	private ObjectMapper objectMapper;
+
+	@BeforeClass
+	private void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+		objectMapper = new ObjectMapper();
+	}
+	
+	@Test
+	public void testOk() throws Exception{
+		String jwt = tokenAuthenticationService.generateToken("amgarcia", "abcd");
+		
+		Ticket ticket = new Ticket();
+		ticket.setPriority(Priority.MEDIUM);
+		ticket.setSubject(StringUtils.random(10));
+		ticket.setDetails("Fixed projector in A309");
+		ticket.setLocation("A309");
+		
+		
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders
+				.put("/tickets/{ticketId}", 1L)
+				.header("Authorization", jwt)
+				.contentType("application/json")
+				.content(objectMapper.writeValueAsString(ticket));
+
+		String res = mockMvc.perform(builder)
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andReturn()
+				.getResponse()
+				.getContentAsString();
+
+		Ticket responseObject = objectMapper.readValue(res, Ticket.class);
+
+		assert responseObject.getId() != null &&
+				responseObject.getPriority() == ticket.getPriority() &&
+				responseObject.getSubject().equals(ticket.getSubject()) &&
+				responseObject.getDetails().equals(ticket.getDetails()) &&
+				responseObject.getLocation().equals(ticket.getLocation());
+	}
+}
