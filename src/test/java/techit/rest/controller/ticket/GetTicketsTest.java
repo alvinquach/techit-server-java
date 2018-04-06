f2e22a58f67f5833c0baae9754986af2994453a4
cowboyuniverse <cowboyuniverse@gmail.com>
2018-04-06 14:46:42 -0700

    cleaning up code

--- src/test/java/techit/rest/controller/ticket/GetTicketsTest.java
+++ src/test/java/techit/rest/controller/ticket/GetTicketsTest.java
@@ -2,7 +2,6 @@ package techit.rest.controller.ticket;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -14,44 +13,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
-
-import static org.hamcrest.Matchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import org.springframework.test.web.servlet.ResultActions;
-
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.Mockito.when;
-
-
-
-
-import static org.junit.Assert.fail;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-
-
-
-import techit.authentication.TokenAuthenticationService;
-import techit.model.Position;
-import techit.model.Priority;
-import techit.model.Ticket;
-import techit.model.Unit;
-import techit.model.User;
-import techit.rest.controller.TicketController;
-import techit.rest.error.RestException;
-import techit.util.StringUtils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -102,11 +63,6 @@ public class GetTicketsTest extends AbstractTransactionalTestNGSpringContextTest
 
 	}
 	
-    @Test
-    public void get() throws Exception {
-//        when(accountServiceMock.getAllAccountNumbers()).thenReturn(Arrays.asList(1, 2));
-//    	MockHttpServletRequestBuilder builder = MockMvcRequestBuilders;
-    }
 
 }
 
@@ -115,7 +71,7 @@ public class GetTicketsTest extends AbstractTransactionalTestNGSpringContextTest
 //TECHNICIAN,
 //USER;
 
-// ---user
+//---user
 //public Position getPosition() {
 //	return position;
 //}
