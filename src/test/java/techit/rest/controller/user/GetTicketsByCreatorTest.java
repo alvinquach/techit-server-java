20626858f88f8ce1fc511ce2c43b39c8844ed107
Unknown <alvinquach91@gmail.com>
2018-04-06 15:29:03 -0700

    added tests to getTicketsByCreator and updateUser

---
+++ src/test/java/techit/rest/controller/user/GetTicketsByCreatorTest.java
@@ -0,0 +1,61 @@
+package techit.rest.controller.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import techit.authentication.TokenAuthenticationService;
+
+@Test(groups = "GetTicketByCreatorTest")
+@WebAppConfiguration
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class GetTicketsByCreatorTest extends AbstractTransactionalTestNGSpringContextTests {
+
+	@Autowired
+	private WebApplicationContext wac;
+
+	@Autowired
+	private TokenAuthenticationService tokenAuthenticationService;
+
+	private MockMvc mockMvc;
+
+	@BeforeClass
+	private void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+	}
+
+	@Test
+	public void testOk() throws Exception{
+		
+		String jwt = tokenAuthenticationService.generateToken("techit", "abcd");
+
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders
+				.get("/users/{userId}/tickets", 3L)
+				.header("Authorization", jwt);
+
+		mockMvc.perform(builder).andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
+	@Test
+	public void testForbidden() throws Exception {
+
+		String jwt = tokenAuthenticationService.generateToken("jdoe", "abcd");
+
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders
+				.get("/users/{userId}/tickets", 3L)
+				.header("Authorization", jwt);
+
+		mockMvc.perform(builder).andExpect(MockMvcResultMatchers.status().isForbidden());
+
+	}
+	
+}
